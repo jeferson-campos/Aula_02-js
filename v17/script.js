4d17b924f6f404b3598bfb671682b4d298460f1c
jeferson-campos <jefersoncampos5296@gmail.com>
2024-06-20 12:15:32 -0300

    Add files via upload

---
+++ v17/script.js
@@ -0,0 +1,32 @@
+//var peso = prompt('digite seu peso em kg')
+//
+//if (peso < 18,5 ) {
+//   console.log('voçê está abaixo do peso')
+//}
+//
+// if (peso >= 18,5 && =< 24,9 ) {
+//    console.log('voçê está com o peso normal')
+//}
+//
+//let = prompt('digite o valor do produto')
+//
+//let = prompt('digite sua idade')
+//
+// else if (idade < 18 ) {//
+//    console.log('voçê ganhou um desconto')
+//}
+ 
+//let = letra (digite uma letra)
+
+// let Numero = prompt('digite um numero')
+
+//   if (numero =>1 && =<7) {
+ //       console.log('vc ganhou um desconto')
+ //   }
+
+// let numero = prompt('digite um numero')
+
+//if ( numero => 1)
+
+//console.log('esse é um numero primo')
+// }
